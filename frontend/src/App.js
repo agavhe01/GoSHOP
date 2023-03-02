b05750ba711eefc29a56e29563fc03ced4d4bd0b
Anesu Gavhera <agavhera@gmail.com>
2023-03-01 22:04:29 -0500

    React Router added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,28 @@
 import { Container } from 'react-bootstrap';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Foooter from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
           <h1> Welcome</h1>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
+
           <HomeScreen />
         </Container>
       </main>
 
       <Foooter />
-    </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProductScreen() {
+  return (
+    <div>
+      <h2>Products</h2>
+    </div>
+  );
+}
+
+export default ProductScreen;
