add488caadf6e7d4d792492a97582f925e96cad2
Anesu Gavhera <agavhera@gmail.com>
2023-03-03 23:52:22 -0500

    JW Web token init, rendered with authorization on backend

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -3,7 +3,15 @@
 from django.urls import path
 from . import views
 
+# JSON Web Token
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
+    path('users/login/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
     path('', views.getRoutes, name="routes"),
     path('products/', views.getProducts, name="products"),
     path('products/<str:pk>', views.getProduct, name="product"),
