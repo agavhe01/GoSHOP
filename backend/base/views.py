30c413fd7e7287551981197c5bda2eaeff0bf67c
Anesu Gavhera <agavhera@gmail.com>
2023-03-04 00:13:53 -0500

    Customized JW Token to include username and email

--- backend/base/views.py
+++ backend/base/views.py
@@ -7,7 +7,23 @@ from .models import Product
 
 from .serializer import ProductSerializer
 
-# Create your views here.
+# JWT
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 
 @api_view(['GET'])
