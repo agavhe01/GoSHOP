8568807f81d7cbce8264778590490dc66f7f57f0
Anesu Gavhera <agavhera@gmail.com>
2023-03-04 18:56:09 -0500

    Signal to set username to email on profile register/update

---
+++ backend/base/signals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import pre_save
+from django.contrib.auth.models import User
+
+# when user model is fired set username to email
+
+
+def updateUser(sender, instance, **kwargs):
+    user = instance
+    if user.email != '':
+        user.username = user.email
+
+
+pre_save.connect(updateUser, sender=User)
