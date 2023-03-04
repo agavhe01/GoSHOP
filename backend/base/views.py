8dd325cd269cf3f25e50434ba3190cc335af26aa
Anesu Gavhera <agavhera@gmail.com>
2023-03-04 18:47:57 -0500

    Register User

--- backend/base/views.py
+++ backend/base/views.py
@@ -13,6 +13,11 @@ from .serializer import ProductSerializer, UserSerializer, UserSerializerWithTok
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+# password encrypt
+from django.contrib.auth.hashers import make_password
+
+from rest_framework import status
+
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
@@ -61,6 +66,24 @@ def getProduct(request, pk):
     return Response(serializer.data)
 
 # ------------ USER -----------------------
+
+
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
+        )
+
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail': 'User with this email already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 
 @permission_classes(IsAuthenticated)
