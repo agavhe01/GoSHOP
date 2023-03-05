2581bc8e8b6fc4d5c69be27e1ad3e20ad5b19566
Anesu Gavhera <agavhera@gmail.com>
2023-03-04 19:52:32 -0500

    Intialized LoginScreen and added route to App router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Foooter from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path='/' component={HomeScreen} exact />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/login' component={LoginScreen} />
         </Container>
       </main>
 
