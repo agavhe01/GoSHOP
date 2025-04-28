0421957340e8e9d72d8429c725e6db2566611948
Anesu Gavhera <agavhera@gmail.com>
2025-04-28 19:05:23 -0400

    set debug to true

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,7 +33,7 @@ if not SECRET_KEY:
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
